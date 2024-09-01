6edc2317f0583e0169c51a7fc7c003edd65c728a
ila36IX <jbariali002@gmail.com>
2024-09-01 09:06:08 +0100

    Unlocking all the boxes is impossible

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+You have n number of locked boxes in front of you. Each box is numbered
+sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+"""
+
+
+def canUnlockAll(boxes):
+    """Check if all the boxes can be unlocked"""
+    check = [False for _ in boxes]
+    check[0] = True
+    canUnlockAll_help(boxes, check, 0)
+    return all(check)
+
+
+def canUnlockAll_help(boxes, check, i):
+    """Check if all the boxes can be unlocked using recursion"""
+    for key in boxes[i]:
+        if check[key] is False:
+            check[key] = True
+            canUnlockAll_help(boxes, check, key)
