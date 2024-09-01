0ddc24b5af2afa0bdef25d84c22da96d70fc6e9b
ila36IX <jbariali002@gmail.com>
2024-09-01 08:15:26 +0100

    pascal traingle is awesome

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,20 +2,24 @@
 """
 Returns a list of lists of integers representing the Pascal’s triangle.
 """
+
+
 def pascal_triangle(n):
     """return list of lists"""
     if n < 0:
         return []
     pascal = [[1]]
     for j in range(1, n):
-        row = [1] + [pascal[j - 1][i] + pascal[j - 1][i + 1] for i in range(j - 1)] + [1]
+        row = [1]
+        row += [pascal[j - 1][i] + pascal[j - 1][i + 1] for i in range(j - 1)]
+        row += [1]
         pascal.append(row)
     return pascal
-# if __name__ == "__main__":
-#     def print_triangle(triangle):
-#         """
-#         Print the triangle
-#         """
-#         for row in triangle:
-#             print("[{}]".format(",".join([str(x) for x in row])))
-#     print_triangle(pascal_triangle(10))
+if __name__ == "__main__":
+    def print_triangle(triangle):
+        """
+        Print the triangle
+        """
+        for row in triangle:
+            print("[{}]".format(",".join([str(x) for x in row])))
+    print_triangle(pascal_triangle(2))
