157c26e590d967ccd2561f1c4e35c80090ec5c7e
ila36IX <jbariali002@gmail.com>
2024-09-21 03:22:31 +0100

    Asynchronous Programming Is Awesome

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 const { argv } = require('node:process');
-const { error } = require('node:console');
 
-const movie_id = argv[2];
+const movieId = argv[2];
 
-function logNames(ListOfNames) {
+function logNames (ListOfNames) {
   ListOfNames.forEach(name => {
     console.log(name);
-  })
+  });
 }
 
-function getNames(charcters) {
+function getNames (charcters) {
   return charcters.map(character => {
     return new Promise((resolve, reject) => {
       request.get(character, (e, r, b) => {
@@ -21,12 +20,12 @@ function getNames(charcters) {
         } catch (error) {
           reject(error);
         }
-      })
-    })
+      });
+    });
   });
 }
 
-url = `https://swapi-api.alx-tools.com/api/films/${movie_id}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 request.get(url, (e, r, b) => {
   if (r.statusCode === 200) {
     const allCharacters = getNames(JSON.parse(b).characters);
@@ -34,4 +33,4 @@ request.get(url, (e, r, b) => {
       .then(names => logNames(names))
       .catch(e => console.error(e));
   }
-})
+});
